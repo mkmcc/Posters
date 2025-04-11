564f7afc10a6876a90951594b4fd58a4524149b4
Michael McCourt <mkmccjr@gmail.com>
2025-04-10 20:36:26 -0700

    add png images for readme

---
+++ README.md
@@ -0,0 +1,5 @@
+![buoyancy poster](./BuoyancySaturation/buoyancy-saturation.png)
+
+![thermal instability poster](./ThermalInstability/thermal-instability.png)
+
+![outer parts poster](./OuterParts/outer-parts.png)
---
+++ convert.rb
@@ -0,0 +1,16 @@
+# want 1200 px images from 11 inch pdfs.
+res = 1200.0 / 11.0
+
+files = [
+  './OuterParts/outer-parts.pdf',
+  './ThermalInstability/thermal-instability.pdf',
+  './BuoyancySaturation/buoyancy-saturation.pdf'
+]
+
+files.each do |file|
+  base = file.chomp(".pdf")
+  cmd = "gs -q -sDEVICE=pngalpha -sBATCH -dSAFER -sNOPAUSE "
+  cmd += "-r#{res} "
+  cmd += "-sOutputFile=#{base}.png #{base}.pdf"
+  system cmd
+end
